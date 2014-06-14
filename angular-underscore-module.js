06c974eedad5f4fd7e38bf96ec544d8bcf646335
michaelmr <michaelmr@users.noreply.github.com>
2014-06-14 00:36:22 -0500

    Add support for underscore inside html views

--- angular-underscore-module.js
+++ angular-underscore-module.js
@@ -2,7 +2,12 @@
  * Let's you use underscore as a service from a controller.
  * Got the idea from: http://stackoverflow.com/questions/14968297/use-underscore-inside-controllers
  * @author: Andres Aguilar https://github.com/andresesfm
+ * Modified following http://stackoverflow.com/a/23984685/2701374
  */
-angular.module('underscore', []).factory('_', function() {
-    return window._; // assumes underscore has already been loaded on the page
-});
+angular.module('underscore', [])
+	// allow DI for use in controllers, unit tests
+	.constant('_', window._)
+	// use in views, ng-repeat="x in _.range(3)"
+	.run(function ($rootScope) {
+	$rootScope._ = window._;
+	});
